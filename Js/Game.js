4ada67e07dacd8e6ea2b7d3e1b82306fcac21639
DimitriBurve <dimilorrain@gmail.com>
2018-11-22 10:38:57 +0100

    ajout fonction 'coucher'

--- Js/Game.js
+++ Js/Game.js
@@ -5,7 +5,8 @@ var Cartes = require('./Cartes.js');
 var readlineSync = require('readline-sync');
 
 function Game() {
-    this.listePlayer = [];
+    this.listePlayerTable = [];
+    this.listePlayerGame = [];
     this.pot = 0;
     this.cartes = new Cartes();
     this.tapisCarte = [];
@@ -16,28 +17,29 @@ function Game() {
 Game.prototype.addPlayer = function(id,name,jetons) {
     let newPlayer = new Player(id, name, jetons);
     newPlayer.game = this;
-    this.listePlayer.push(newPlayer);
+    this.listePlayerTable.push(newPlayer);
+    this.listePlayerGame.push(newPlayer);
 };
 
 Game.prototype.reset = function() {
     this.pot = 0;
     this.cartes = new Cartes();
-    for (let i=0; i<this.listePlayer.length; i++) {
-        this.listePlayer[i].reset();
+    for (let i=0; i<this.listePlayerGame.length; i++) {
+        this.listePlayerGame[i].reset();
     }
     this.tour=0;
 };
 
 Game.prototype.blind = function(petiteBlinde, grosseBlinde){
 
-    if (this.dealer>=this.listePlayer.length){
+    if (this.dealer>=this.listePlayerGame.length){
         this.dealer=0;
     }
-    this.listePlayer[this.dealer+1].fold(petiteBlinde);
-    if (this.dealer+2<this.listePlayer.length) {
-        this.listePlayer[this.dealer + 2].fold(grosseBlinde);
+    this.listePlayerGame[this.dealer+1].fold(petiteBlinde);
+    if (this.dealer+2<this.listePlayerGame.length) {
+        this.listePlayerGame[this.dealer + 2].fold(grosseBlinde);
     }else {
-        this.listePlayer[0].fold(grosseBlinde);
+        this.listePlayerGame[0].fold(grosseBlinde);
     }
     this.pot=petiteBlinde+grosseBlinde;
     this.dealer++;
@@ -58,8 +60,8 @@ Game.prototype.affichage = function(){
 
     console.log();
     console.log('Joueurs :');
-    for (let i=0;i<this.listePlayer.length;i++){
-        console.log(''+this.listePlayer[i].getNom()+" : "+this.listePlayer[i].getMain()+", "+this.listePlayer[i].getJetons()+" coins");
+    for (let i=0;i<this.listePlayerGame.length;i++){
+        console.log(''+this.listePlayerGame[i].getNom()+" : "+this.listePlayerGame[i].getMain()+", "+this.listePlayerGame[i].getJetons()+" coins");
     }
 };
 
@@ -69,10 +71,11 @@ Game.prototype.affichage = function(){
  */
 Game.prototype.option = function(miseMin){
 
+    let index =[];
     let actionPrec = 'check'; //action précedente autre que coucher pour déterminer possibilités des actions a jouer
 
-    for (let i=0;i<this.listePlayer.length;i++) {
-        console.log(this.listePlayer[i].getNom()+' :');
+    for (let i=0;i<this.listePlayerGame.length;i++) {
+        console.log(this.listePlayerGame[i].getNom()+' :');
         let action;
 
 
@@ -87,11 +90,11 @@ Game.prototype.option = function(miseMin){
 
                 break;
             case 'relancer':
-                if (miseMin >this.listePlayer[i].getJetons()){
+                if (miseMin >this.listePlayerGame[i].getJetons()){
                     do {
                         action = readlineSync.question('action : coucher, all-in : ');
                     } while (action === 'check' || action === 'relancer' || action ==='suivre');
-                }else if(miseMin === this.listePlayer[i].getJetons()){
+                }else if(miseMin === this.listePlayerGame[i].getJetons()){
                     do {
                         action = readlineSync.question('action : coucher, mise min/suivre(' + miseMin + '), all-in : ');
                     } while (action === 'check' || action === 'relancer');
@@ -113,29 +116,30 @@ Game.prototype.option = function(miseMin){
 
         switch (action) {
             case 'coucher':
-                this.listePlayer[i].coucher();
+                this.listePlayerGame[i].coucher();
+                index.push(i);
                 break;
             case 'check':
-                this.listePlayer[i].callCheck();
+                this.listePlayerGame[i].callCheck();
                 actionPrec = 'check';
                 break;
             case 'suivre':
-                this.listePlayer[i].fold(miseMin);
+                this.listePlayerGame[i].fold(miseMin);
                 this.pot+=miseMin;
                 actionPrec = 'suivre';
                 break;
             case 'relancer':
                 do {
                     var miseRel = readlineSync.question('mise Relance : ');
-                } while (miseRel >this.listePlayer[i].getJetons() || miseRel <= miseMin);
+                } while (miseRel >this.listePlayerGame[i].getJetons() || miseRel <= miseMin);
                 miseMin = miseRel;
                 this.fold(miseMin);
                 this.pot+=miseMin;
                 actionPrec = 'relancer';
                 break;
             case 'all-in':
-                this.listePlayer[i].allin();
-                miseMin = this.listePlayer[i].getTas();
+                this.listePlayerGame[i].allin();
+                miseMin = this.listePlayerGame[i].getTas();
                 this.pot += miseMin;
                 actionPrec = 'all-in';
                 break;
@@ -144,10 +148,15 @@ Game.prototype.option = function(miseMin){
         }
         console.log();
     }
+
+    for (i=0;i<index.length;i++){
+        this.listePlayerGame.splice(i+1,1);
+    }
+
 };
 
 Game.prototype.play = function (petiteBlinde, grosseBlinde) {
-    while (this.listePlayer.length > 1){
+    while (this.listePlayerGame.length > 1){
         this.reset();
 
         // initialisation des blinds
@@ -157,8 +166,8 @@ Game.prototype.play = function (petiteBlinde, grosseBlinde) {
 
         //initialisation main + affichage main joueur
         for (let i=0 ; i<2 ;i++){
-            for (let j=0 ; j<this.listePlayer.length ;j++){
-                this.listePlayer[j].addMain(this.cartes.giveCarte());
+            for (let j=0 ; j<this.listePlayerGame.length ;j++){
+                this.listePlayerGame[j].addMain(this.cartes.giveCarte());
             }
 
         }
