0837a4f777f7be2eaf7bde41403f7e6bff669cc9
jn-robert <37399778+jn-robert@users.noreply.github.com>
2018-12-13 17:05:03 +0100

    Add files via upload

---
+++ Poker_BDD.sql
@@ -0,0 +1,62 @@
+DROP TABLE IF EXISTS Player;
+DROP TABLE IF EXISTS Partie;
+DROP TABLE IF EXISTS Classement;
+DROP TABLE IF EXISTS Mise;
+DROP TABLE IF EXISTS Gain;
+DROP TABLE IF EXISTS Action;
+
+CREATE TABLE Player (
+	idPlayer INT AUTO_INCREMENT,
+	nom VARCHAR(20),
+	prenom VARCHAR(20),
+	pseudo VARCHAR(15),
+	dateInscription DATE,
+	jetons INT,
+	PRIMARY KEY(idPlayer)
+);
+
+CREATE TABLE Partie (
+	idPartie INT AUTO_INCREMENT,
+	typePartie VARCHAR(20),
+	nbTour INT,
+	nbJoueur INT,
+	PRIMARY KEY(idPartie)
+);
+
+CREATE TABLE Classement (
+	idPartie INT,
+	idPlayer INT,
+	class INT,
+	PRIMARY KEY(idPartie, idPlayer),
+	CONSTRAINT fkClassPartie FOREIGN KEY (idPartie) REFERENCES Partie(idPartie),
+	CONSTRAINT fkClassPlayer FOREIGN KEY (idPlayer) REFERENCES Player(idPlayer)
+);
+
+CREATE TABLE Mise (
+	idPlayer INT,
+	idPartie INT,
+	mise INT,
+	PRIMARY KEY(idPlayer, idPartie),
+	CONSTRAINT fkMisePlayer FOREIGN KEY (idPlayer) REFERENCES Player(idPlayer),
+	CONSTRAINT fkMisePartie FOREIGN KEY (idPartie) REFERENCES Partie(idPartie)
+);
+
+CREATE TABLE Gain (
+	idPartie INT,
+	idPlayer INT,
+	gain INT,
+	PRIMARY KEY(idPlayer, idPartie),
+	CONSTRAINT fkMisePlayer FOREIGN KEY (idPlayer) REFERENCES Player(idPlayer),
+	CONSTRAINT fkMisePartie FOREIGN KEY (idPartie) REFERENCES Partie(idPartie)
+);
+
+CREATE TABLE Action (
+	idPartie INT,
+	idPlayer INT,
+	nbAllIn INT,
+	nbCheck INT,
+	nbFold INT,
+	PRIMARY KEY(idPlayer, idPartie),
+	CONSTRAINT fkMisePlayer FOREIGN KEY (idPlayer) REFERENCES Player(idPlayer),
+	CONSTRAINT fkMisePartie FOREIGN KEY (idPartie) REFERENCES Partie(idPartie)
+);
