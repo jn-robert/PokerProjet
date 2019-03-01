a56081916a23a15d3b3b8232fd12cbee73fa7f5b
Victooor68 <victor.brungard@gmail.com>
2019-03-01 14:12:42 +0100

    Add files via upload
    maj home

--- fonction.js
+++ fonction.js
@@ -11,6 +11,22 @@ function begin(){
     document.getElementsByClassName("Game")[0].style.display = "none";
 }
 
+function showLogin(){
+    clear();
+    document.getElementsByClassName("login")[0].style.display = "block";
+}
+
+function showRegister(){
+    clear();
+    document.getElementsByClassName("register")[0].style.display = "block";
+}
+
+function clear(){
+    document.getElementsByClassName("login")[0].style.display = "none";
+    document.getElementsByClassName("register")[0].style.display = "none";
+    document.getElementsByClassName("home")[0].style.display = "none";
+}
+
 
 /*
 $(document).ready(function() {
